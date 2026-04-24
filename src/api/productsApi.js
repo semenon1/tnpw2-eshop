ae8b36ea29543cb6db2c89dcdc06ca8d5c4ad6f0
semenon1 <semenon1@uhk.cz>
2026-04-24 13:47:25 +0200

    added outOfStocks product status

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,7 +1,7 @@
-//imporuje funkci z utils.js
+// imporuje funkci z utils.js
 import { delay } from "./utils.js";
 
-//public funkce k dispozici pro ostatní soubory
+// public funkce k dispozici pro ostatní soubory
 export function createProductsApi(db) {
   return {
     
@@ -11,6 +11,45 @@ export function createProductsApi(db) {
       return {
         status: "SUCCESS",
         products: structuredClone(db.products)
+      };
+    },
+
+
+    async decreaseStock(productId, amount = 1) {
+      await delay();
+
+      const product = db.products.find((p) => p.id === productId);
+
+      if (!product) {
+        return { 
+          status: "REJECTED", 
+          reason: "Produkt neexistuje." 
+        };
+      }
+
+      if (product.status === "ARCHIVED") {
+        return { 
+          status: "REJECTED", 
+          reason: "Vyřazené produkty nelze kupovat." 
+        };
+      }
+
+      if (product.stockCount < amount) {
+        return { 
+          status: "REJECTED", 
+          reason: "Nedostatek zboží na skladě." 
+        };
+      }
+
+      product.stockCount -= amount;
+
+      if (product.stockCount === 0) {
+        product.status = "OUT_OF_STOCK";
+      }
+
+      return { 
+        status: "SUCCESS", 
+        product: structuredClone(product) 
       };
     }
 
